14450e83e2223839b939a2811e0b8651e1eb788f
Brais Lopez <braislopezz05@gmail.com>
2026-04-25 19:14:09 +0200

    Configure Spring Boot backend with PostgreSQL

---
+++ backend/gymtrack/src/main/java/com/brais/gymtrack/GymtrackApplication.java
@@ -0,0 +1,13 @@
+package com.brais.gymtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtrackApplication.class, args);
+	}
+
+}
---
+++ backend/gymtrack/src/test/java/com/brais/gymtrack/GymtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brais.gymtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
